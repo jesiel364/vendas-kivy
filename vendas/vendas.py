9c43316ff0f9a8ed1be422a89b36d0caac773708
jesiel364 <jesiel364@gmail.com>
2022-04-06 01:06:23 -0400

    Layout quase completo

--- vendas/vendas.py
+++ vendas/vendas.py
@@ -5,10 +5,38 @@ class VendasWindow(BoxLayout):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 
+	def admin(self):
+		pass
+
+	def sair(self):
+		pass
+
+	def add_produto_codigo(self,codigo):
+		pass
+
+	def add_produto_nome(self, nome):
+		pass
+
+	def atualizar(self):
+		pass
+
+	def deletar(self):
+		pass
+
+	def pagar(self):
+		pass
+
+	def nova_compra(self):
+		pass
+
 
 class VendasApp(App):
 	def build(self):
 		return VendasWindow()
+
+	
+
+
 
 
 if __name__ == '__main__':
